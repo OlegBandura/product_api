f2f3cbf76285752303e342839f519bf1408cf72f
Oleg <oleg.bandura@onapp.com>
2018-11-12 19:42:00 +0200

    create DB and table. Create seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do
+  Product.create(
+    [
+      {
+        name: Faker::Food.vegetables,
+        description: Faker::Food.description,
+        price: rand(1..10000)
+      }
+    ]
+  )
+end
